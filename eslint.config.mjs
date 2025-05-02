59416e1231ea2dbe91a2b1742d6dfbfcdc460a7e
abhishek7088 <abhi7088721935@gmail.com>
2025-05-02 10:29:41 +0530

    server file is refactored again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,15 +8,7 @@ const __dirname = dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
-async function fetchData() {
-  try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
-    const data = await response.json();
-    console.log('Fetched Data:', mmuthu);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-}
+
 
 fetchData();
 const eslintConfig = [
