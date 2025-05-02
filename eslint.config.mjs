1d9a074537e72ad9c09274f3d8cad9ae3093fc85
abhishek7088 <abhi7088721935@gmail.com>
2025-05-02 10:18:46 +0530

    modified something

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,17 @@ const __dirname = dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
- sdjcsdclkns lskdmlclkmsmc
+async function fetchData() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+    const data = await response.json();
+    console.log('Fetched Data:', muthu);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+fetchData();
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
