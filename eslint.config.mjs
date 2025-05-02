2ae1e8a10555961942108441bda02c98fd81c430
abhishek7088 <abhi7088721935@gmail.com>
2025-05-02 10:20:25 +0530

    something good

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ async function fetchData() {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
     const data = await response.json();
-    console.log('Fetched Data:', muthu);
+    console.log('Fetched Data:', mmuthu);
   } catch (error) {
     console.error('Error fetching data:', error);
   }
